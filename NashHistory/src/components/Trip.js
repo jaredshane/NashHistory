10f4e2ef6195e1a994711ebe8103b166fcaa0f2a
Jared Fuller <jaredshane@gmail.com>
2017-05-15 19:21:02 -0500

    added functions to save a trip to the database and a function to show the save button if the user is logged in

--- NashHistory/src/components/Trip.js
+++ NashHistory/src/components/Trip.js
@@ -35,6 +35,39 @@ navigatePlace(la, lo) {
   return Linking.openURL(url)
 }
 
+saveTrips() {
+  console.log(this.state.trip)
+  Promise.all(this.state.trip.map(trip => {
+    console.log(trip)
+    const data = {
+      latitude: trip.latitude,
+      longitude: trip.longitude,
+      title: trip.title,
+      subtitle: trip.subtitle,
+      number: trip.number,
+      location: trip.location,
+      user_id: this.props.id }
+    return axios.post('http://localhost:3000/v1/trip', data)
+    .then((res) => {
+      console.log(res)
+    })
+  }))
+}
+
+showSave() {
+  if (this.props.email) {
+    return (
+      <TouchableHighlight
+        onPress={this.saveTrips}
+      >
+        <Text>
+          Save Your Trip!
+        </Text>
+      </TouchableHighlight>
+    )
+  }
+}
+
 renderTrips() {
   return this.props.tripList.map(trip => {
     console.log('trip', trip)
