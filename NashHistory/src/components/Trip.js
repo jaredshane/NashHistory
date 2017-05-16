6f784160f88a9cbbab24ac881fb7c49931352790
Jared Fuller <jaredshane@gmail.com>
2017-05-15 19:19:04 -0500

    removed Button from the import and imported axios

--- NashHistory/src/components/Trip.js
+++ NashHistory/src/components/Trip.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text, Button, TouchableHighlight, Linking } from 'react-native'
+import { View, Text, TouchableHighlight, Linking } from 'react-native'
+import axios from 'axios'
 
 class Trip extends Component {
 
